b4d9180d79e770e9514ca908820b9f95088e3746
Hitendra iDev <hitendra.developer@gmail.com>
2018-05-24 00:17:20 +0530

    solution done

---
+++ HSTimeRange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HSTimeRange
+//
+//  Created by Hitendra iDev on 23/05/18.
+//  Copyright © 2018 Hitendra iDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
